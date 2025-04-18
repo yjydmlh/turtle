c21315e240fc9b8c81890190a2f9eb8d6d2ad7a6
yjydmlh <yjydmlh@gmail.com>
2025-04-18 17:33:17 +0800

    测试

--- app/scripts/test_kline_query.py
+++ app/scripts/test_kline_query.py
@@ -2,9 +2,10 @@ from datetime import datetime, timedelta
 
 import psycopg2
 
-from app.db.session import SessionLocal
+from app.db.session import SessionLocal, get_db
 from app.crud import kline
 from app.core.logger import app_logger
+from app.models.kline import BtcUsdtKline
 
 
 def test_kline_query(symbol: str = "btc_usdt", days: int = 7) -> None:
@@ -137,30 +138,36 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.exc import DatabaseError
 from app.db.session import engine
 from app.core.config import settings
+from sqlalchemy import select
 
 if __name__ == "__main__":
+    stmt = select(BtcUsdtKline).limit(10)
+    with SessionLocal() as db:
+        for kline in db.scalars(stmt):
+            print("原始数据:", vars(kline))
+
     # engine = create_engine("postgresql://postgres:123456@127.0.0.1:5532/postgres")
-    engine = create_engine(
-        settings.DATABASE_URL
-        , pool_pre_ping=True  # 自动检测连接是否有效
-        , pool_size=5  # 连接池大小
-        , max_overflow=10  # 最大溢出连接数
-        , pool_timeout=30  # 连接池超时时间
-        , pool_recycle=1800  # 连接回收时间（秒）
-        , echo=settings.DEBUG  # 在调试模式下打印SQL语句
-    )
-    try:
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT * FROM btc_usdt_copy1 limit 10"))
-            for idx, row in enumerate(result):
-                try:
-                    # 强制将每个字段转为字符串（触发解码）
-                    print(f"Row {idx}: {[str(cell) for cell in row]}")
-                except UnicodeDecodeError as e:
-                    print(f"解码错误行号: {idx}, 错误信息: {e}")
-                    print("原始数据:", row)
-    except DatabaseError as e:
-        print("数据库错误:", e)
+    # engine = create_engine(
+    #     settings.DATABASE_URL
+    #     , pool_pre_ping=True  # 自动检测连接是否有效
+    #     , pool_size=5  # 连接池大小
+    #     , max_overflow=10  # 最大溢出连接数
+    #     , pool_timeout=30  # 连接池超时时间
+    #     , pool_recycle=1800  # 连接回收时间（秒）
+    #     , echo=settings.DEBUG  # 在调试模式下打印SQL语句
+    # )
+    # try:
+    #     with engine.connect() as conn:
+    #         result = conn.execute(text("SELECT * FROM btc_usdt_copy1 limit 10"))
+    #         for idx, row in enumerate(result):
+    #             try:
+    #                 # 强制将每个字段转为字符串（触发解码）
+    #                 print(f"Row {idx}: {[str(cell) for cell in row]}")
+    #             except UnicodeDecodeError as e:
+    #                 print(f"解码错误行号: {idx}, 错误信息: {e}")
+    #                 print("原始数据:", row)
+    # except DatabaseError as e:
+    #     print("数据库错误:", e)
 
     # # 运行基本测试
     # test_kline_query()
