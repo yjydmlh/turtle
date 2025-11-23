2bd60c46e78695813888621870379c1b5b45286e
yjydmlh <yjydmlh@gmail.com>
2025-11-23 22:54:31 +0800

    data

---
+++ app/data/binance/BinanceIntervalEnum.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class BinanceIntervalEnum(Enum):
+    ONE_MINUTE = '1m'
+    THREE_MINUTES = '3m'
+    FIVE_MINUTES = '5m'
+    FIFTEEN_MINUTES = '15m'
+    THIRTY_MINUTES = '30m'
+    ONE_HOUR = '1h'
+    TWO_HOURS = '2h'
+    FOUR_HOURS = '4h'
+    SIX_HOURS = '6h'
+    EIGHT_HOURS = '8h'
+    TWELVE_HOURS = '12h'
+    ONE_DAY = '1d'
+    THREE_DAYS = '3d'
+    ONE_WEEK = '1w'
+    ONE_MONTH = '1M'
+
+    def __str__(self):
+        return self.value
